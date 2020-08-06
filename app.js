4bab5b212221af26690159c85a8adff83377c835
springlordpants <springlordpants@gmail.com>
2020-08-06 11:27:31 -0700

    Site built
    Javascript complete

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('JS File loaded')
+$(document).ready(function(){
+
+  $('#get-pokemon-button').on('click',function(){
+
+    $.get('https://pokeapi.co/api/v2/pokemon/', function(data, status) {
+
+      var string = "";
+
+      string += "<table class='table'>";
+
+      $.each(data.results, function(index, item){
+        string +="<tr><td><a href='"+item.url+"'>"+item.name+"</td><td><button class='hide-button btn btn-primary'>Hide</button></td></tr>";
+      });
+
+      string += "</table>";
+
+      $('#poke-list').html(string);
+
+      if(status) {
+        $('.hide-button').on('click',function() {
+          $(this).parent().parent().hide();
+        });
+      }
+    });
+
+  });
+
+});
